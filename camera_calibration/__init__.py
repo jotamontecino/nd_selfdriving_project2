22ff96a6336bf562ba30f0bc1ee54d602c8afa7b
Javier Venegas <jota.montecino@gmail.com>
2019-06-19 10:30:57 +0200

    MOD: début du pipeline images

--- camera_calibration/__init__.py
+++ camera_calibration/__init__.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import matplotlib.image as mpimg
 import re
 
@@ -6,13 +7,14 @@ from common.logger import Logger
 from camera_calibration.calibrationBuilder import calibrationMatrix
 from camera_calibration.undistort import builder
 
+
 # Returns a closure undistorting images with the calculated matrix
 # and distortion parameters in the calibration part
 def undistortImageBuilder():
-    log = Logger.getInstance();
+    log = Logger.getInstance()
     log.info(">>> Camera calibration")
-    assetsFolder = os.environ['ASSETS_FOLDER'];
-    cameraCalibrationFolder = "%s/camera_cal"%(assetsFolder);
+    assetsFolder = os.environ['ASSETS_FOLDER']
+    cameraCalibrationFolder = "%s/camera_cal" % (assetsFolder)
     ret, mtx, dist, rvecs, tvecs = calibrationMatrix(9, 6, cameraCalibrationFolder)
     log.info("<<< Camera calibration")
     undistortImage = builder(mtx, dist)
@@ -21,8 +23,8 @@ def undistortImageBuilder():
         fileList = os.listdir(cameraCalibrationFolder)
         log.info(">>> Undistorting camera calibration images")
         for filename in fileList:
-            if (re.search("^[a-z0-9]+\.(jpg|png)$", filename)):
-                currentImage = mpimg.imread("%s/%s"%(cameraCalibrationFolder,filename))
+            if (re.search(r"^[a-z0-9]+\.(jpg|png)$", filename)):
+                currentImage = cv2.imread("%s/%s"%(cameraCalibrationFolder,filename))
                 currentImagePath = "%s/undistorted-%s"%(cameraCalibrationFolder,filename)
                 undistortImage(currentImage, currentImagePath);
         log.info("<<< Undistorting camera calibration images")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
+import cv2
 import matplotlib.image as mpimg
+import os
 import re
 
 from common import logger
@@ -9,5 +10,17 @@ if __name__ == '__main__':
     # logging.basicConfig(filename='myapp.log', level=logging.INFO)
     log = logger.Logger(os.environ['PYTHON_ENV'])
     log.info("Process Start")
-    
+
     undistortImage = undistortImageBuilder()
+
+    if (os.environ['INPUT_TYPE'] == "video"):
+        pass
+    else:
+        assetsFolder = os.environ['ASSETS_FOLDER']
+        testImageFolder = "%s/test_images" % (assetsFolder)
+        fileList = os.listdir(testImageFolder)
+        for filename in fileList:
+            if (re.search(r"^[a-z0-9_]+\.(jpg|png)$", filename)):
+                currentImage = cv2.imread("%s/%s" % (testImageFolder, filename))
+                currentImagePath = "%s/undistorted-%s" % (testImageFolder, filename)
+                undistortImage(currentImage, currentImagePath)
